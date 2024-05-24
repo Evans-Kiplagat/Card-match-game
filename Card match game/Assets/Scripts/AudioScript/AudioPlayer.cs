c5595519a2a1f61360235f79dc66ce6eef97ecff
Evans kiplagat <31707324+Evans-Kiplagat@users.noreply.github.com>
2024-05-24 15:01:45 +0300

    added image-sprite and scripts

---
+++ Card match game/Assets/Scripts/AudioScript/AudioPlayer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CardMatchGame
+{
+    public class AudioPlayer : MonoBehaviour
+    {
+        private static AudioPlayer instance;
+        [SerializeField]
+        private AudioSource audioSourceComponent;
+        [SerializeField]
+        private AudioClip[] audioClips;
+        private float volume = 1f;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                audioSourceComponent = GetComponent<AudioSource>();
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public static AudioPlayer Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void PlayAudioClip(int clipIndex)
+        {
+            if (audioClips != null && clipIndex >= 0 && clipIndex < audioClips.Length)
+            {
+                audioSourceComponent.PlayOneShot(audioClips[clipIndex], volume);
+            }
+            else
+            {
+                Debug.LogWarning($"AudioPlayer: Invalid audio clip index {clipIndex}");
+            }
+        }
+
+        public void PlayAudioClip(int clipIndex, float volumeLevel)
+        {
+            if (audioClips != null && clipIndex >= 0 && clipIndex < audioClips.Length)
+            {
+                audioSourceComponent.PlayOneShot(audioClips[clipIndex], volumeLevel);
+            }
+            else
+            {
+                Debug.LogWarning($"AudioPlayer: Invalid audio clip index {clipIndex}");
+            }
+        }
+
+        public void SetAudioClips(AudioClip[] newAudioClips)
+        {
+            audioClips = newAudioClips;
+        }
+
+        public void SetVolume(float newVolume)
+        {
+            volume = newVolume;
+        }
+    }
+
+}
+
+   
+   
